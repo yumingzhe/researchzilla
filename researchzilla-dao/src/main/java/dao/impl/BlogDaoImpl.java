b1d72acc6fb3ac71e0eb2cea39a44181c6ee064b
yumingzhe <yumingzhe@live.cn>
2012-05-24 15:34:52 +0800

    add  activity service

--- researchzilla-dao/src/main/java/dao/impl/BlogDaoImpl.java
+++ researchzilla-dao/src/main/java/dao/impl/BlogDaoImpl.java
@@ -1,5 +1,6 @@
-package dao;
+package dao.impl;
 
+import dao.BlogDao;
 import org.hibernate.SessionFactory;
 import org.springframework.orm.hibernate3.HibernateTemplate;
 import pojo.Blog;
---
+++ researchzilla-service/src/main/java/service/ActivityService.java
@@ -0,0 +1,14 @@
+package service;
+
+import pojo.Activity;
+
+import java.io.Serializable;
+
+/**
+ * User: yumingzhe
+ * Date: 5/24/12
+ * Time: 3:29 PM
+ */
+public interface ActivityService {
+    public Serializable saveActivity(Activity activity);
+}
