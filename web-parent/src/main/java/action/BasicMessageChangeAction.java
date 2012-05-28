9d7121c0e723835d6ea07aae4e37651fa5c80de5
wangyan <wangyan_megan@yeah.net>
2012-05-28 17:27:40 +0800

    update struts.xml and spring-config.xml

---
+++ web-parent/src/main/java/action/BasicMessageChangeAction.java
@@ -0,0 +1,11 @@
+package action;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 12-5-28
+ * Time: 下午5:03
+ * To change this template use File | Settings | File Templates.
+ */
+public class BasicMessageChangeAction {
+}
